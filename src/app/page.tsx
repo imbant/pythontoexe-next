b40c348ba76d143c7c0499b0b079d1f4b9d93766
Xu Mingyu <13634731015@sina.cn>
2024-01-30 16:43:11 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,91 @@
-import Image from "next/image";
+"use client";
+
+import { FormEventHandler, useState, useEffect } from "react";
+import dynamic from 'next/dynamic';
+import { Spin } from 'antd'
+
+const PythonEditor = dynamic(() => import('@/components/monaco'), {
+  ssr: false, // 禁用服务端渲染
+});
+
+
+const defaultSubmitText = "Get Started";
 
 export default function Home() {
+  const [sourceCode, setSourceCode] = useState<string>("");
+  const [submitText, setSubmitText] = useState<string>(defaultSubmitText);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      setSubmitText("Loading...");
+    } else {
+      setSubmitText(defaultSubmitText);
+    }
+  }, [isLoading])
+
+  const handleSubmit = async () => {
+    if (isLoading) {
+      return
+    }
+
+    setIsLoading(true);
+
+    let blob: Blob;
+    try {
+      blob = await fetch("http://139.224.103.165:3000", {
+        method: "POST",
+        body: JSON.stringify({ sourceCode }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.blob());
+    } catch (error) {
+      setIsLoading(false);
+
+      console.error(error)
+    }
+
+    // 创建 Blob URL
+    const url = window.URL.createObjectURL(blob!);
+    // 创建一个链接元素
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    // 设置下载的文件名
+    a.download = "PythonToExe.exe"; // 你希望保存的文件名
+    // 将链接元素添加到页面并点击它
+    document.body.appendChild(a);
+    a.click();
+    // 移除链接元素并释放 Blob URL
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+
+    setIsLoading(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <header>
+        <div className="mx-auto max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+          <div className="mx-auto mb-8 w-full max-w-3xl text-center md:mb-12 lg:mb-16">
+            <h1 className="mb-4 text-4xl font-bold md:text-6xl">Python to EXE: Simplify Your Coding Journey!</h1>
+            <p className="mx-auto mb-6 max-w-lg text-base text-[#636262] md:mb-10 lg:mb-12">No Tech Background? No Problem! Easily Create Standalone Applications from Your Python Scripts and Share Your Achievements with the World!</p>
+            <div className="flex items-center justify-center">
+              <a onClick={handleSubmit} href="#" className="mr-5 flex items-center bg-[#276ef1] px-8 py-4 font-semibold text-white transition [box-shadow:rgb(171,_196,245)-8px_8px] hover:[box-shadow:rgb(171,_196,_245)_0px_0px] md:mr-6 lg:mr-8">
+                <p className="mr-6 font-bold">{submitText}</p>
+              </a>
+              <a href="https://imbant.github.io/blog/about/" className="flex font-bold">
+                <p className="text-black">about author</p>
+              </a>
+            </div>
+          </div>
+          <Spin spinning={isLoading}>
+            <PythonEditor onChange={(input: string) => setSourceCode(input)} />
+          </Spin>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ src/components/monaco/index.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+import * as monaco from 'monaco-editor';
+
+const PythonEditor = (props: { onChange: (input: string) => void }) => {
+    const editorRef = useRef(null);
+    const editorInstance = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);  // 用于存储编辑器实例
+
+    useEffect(() => {
+        if (editorRef.current) {
+            const defaultValue = "# input your python code \nprint('Hello, Python To EXE!')"
+            props.onChange(defaultValue);
+
+            editorInstance.current = monaco.editor.create(editorRef.current, {
+                value: defaultValue,
+                language: 'python',
+                theme: 'vs-dark',
+            });
+
+            // 获取用户的代码
+            const getCode = () => {
+                const code = editorInstance.current?.getValue();
+                props.onChange(code ?? '');
+            };
+
+            // 示例：每次内容变化时打印代码
+            editorInstance.current?.onDidChangeModelContent(() => {
+                getCode();
+            });
+        }
+        return () => {
+            editorInstance.current?.dispose();
+        };
+    }, []);
+
+    return <div ref={editorRef} style={{ height: '400px' }} />;
+};
+
+export default PythonEditor;
